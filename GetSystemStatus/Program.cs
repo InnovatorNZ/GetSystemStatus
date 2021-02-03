abac4bbdeb95ff212f1532e0d20922880d289519
InnovatorNZ2@outlook.com <InnovatorNZ2@outlook.com>
2021-02-03 18:03:21 +0800

    Modified and Update, added network interface.

--- GetSystemStatus/Program.cs
+++ GetSystemStatus/Program.cs
@@ -7,8 +7,11 @@ using System.Text;
 using System.Management;
 using System.Runtime.InteropServices;
 using System.Web;
-using Amazon.EC2.Model;
+//using Amazon.EC2.Model;
 using System.Runtime.CompilerServices;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 
 namespace GetSystemStatus
 {
@@ -16,26 +19,31 @@ namespace GetSystemStatus
     {
         static void Main(string[] args)
         {
-            SystemInfo systemInfo = new SystemInfo();
-            for (int _ = 0; _ < 30; _++)
+            SystemInfo sysInfo = new SystemInfo();
+            for (int _ = 0; _ < 3000; _++)
             {
                 Console.Clear();
-                Console.WriteLine("C# Get System Status Task Manager Console Edition:");
-                int cusage = (int)Math.Round(systemInfo.CpuLoad);
-                Console.Write("CPU Total Usage: " + cusage + "%\t");
-                for (int i = 0; i < systemInfo.ProcessorCount; i++)
+                Console.WriteLine("Task Manager Console Edition");
+                int cusage = (int)Math.Round(sysInfo.CpuLoad);
+                Console.Write("CPU Total Usage: " + cusage + "%\n");
+                for (int i = 0; i < sysInfo.ProcessorCount; i++)
                 {
-                    int ccusage = (int)Math.Round(systemInfo.CpuCoreLoad(i));
-                    Console.Write("Core " + i + " Usage: " + ccusage + "%  ");
+                    int ccusage = (int)Math.Round(sysInfo.CpuCoreLoad(i));
+                    Console.Write("Core " + i + " Usage: " + ccusage + "%\t");
                 }
-                int rusage = (int)Math.Round((1.0 - (double)systemInfo.MemoryAvailable / (double)systemInfo.PhysicalMemory) * 100.0);
                 Console.WriteLine();
-                Console.WriteLine("RAM Usage: " + rusage + "%\t");
-                for (int i = 0; i < systemInfo.m_DiskNum; i++)
+                int rusage = (int)Math.Round((1.0 - (double)sysInfo.MemoryAvailable / (double)sysInfo.PhysicalMemory) * 100.0);
+                string[] scale_unit = { "Bytes", "KB", "MB", "GB", "TB" };
+                int ramScale = (int)Math.Floor(Math.Log((double)sysInfo.MemoryAvailable, 1024));
+                double memAvail = Math.Round((double)sysInfo.MemoryAvailable / Math.Pow(1024, ramScale), 1);
+                double memTotal = Math.Round((double)sysInfo.PhysicalMemory / Math.Pow(1024, ramScale), 1);
+                Console.WriteLine("RAM Usage: {0}/{1}{2} ({3}%)", memTotal - memAvail, memTotal, scale_unit[ramScale], rusage);
+                for (int i = 0; i < sysInfo.m_DiskNum; i++)
                 {
                     //float fDiskRead = systemInfo.DiskReadTotal;
-                    float fDiskRead = systemInfo.DiskRead(i);
-                    float fDiskWrite = systemInfo.DiskWriteTotal;
+                    float fDiskRead = sysInfo.DiskRead(i);
+                    //float fDiskWrite = systemInfo.DiskWriteTotal;
+                    float fDiskWrite = sysInfo.DiskWrite(i);
                     int rscale = (int)Math.Floor(Math.Log(fDiskRead, 1024));
                     int wscale = (int)Math.Floor(Math.Log(fDiskWrite, 1024));
                     if (rscale < 0) rscale = 0;
@@ -45,12 +53,52 @@ namespace GetSystemStatus
                     string[] speed_units = { "Bytes/sec", "KB/s", "MB/s", "GB/s", "TB/s" };
                     fDiskRead = (float)Math.Round(fDiskRead, 1);
                     fDiskWrite = (float)Math.Round(fDiskWrite, 1);
-                    Console.Write("Disk " + i + " Read : " + fDiskRead + speed_units[rscale] + "\t");
-                    Console.Write("Disk Write Total: " + fDiskWrite + speed_units[wscale] + "\t");
+                    string cDiskDesc = "Disk " + i + " ";
+                    string[] csplit = sysInfo.DiskInstanceNames[i].Split(' ');
+                    if (csplit.Length > 1)
+                    {
+                        cDiskDesc += "(";
+                        for (int j = 1; j < csplit.Length - 1; j++)
+                            cDiskDesc += csplit[j] + " ";
+                        cDiskDesc += csplit[csplit.Length - 1];
+                        cDiskDesc += ")";
+                    }
+                    Console.Write(cDiskDesc);
+                    Console.Write(" Load: " + (int)Math.Round(sysInfo.DiskLoad(i)) + "%");
+                    Console.Write("\tRead: " + fDiskRead + speed_units[rscale]);
+                    Console.Write("\tWrite: " + fDiskWrite + speed_units[wscale]);
                     Console.WriteLine();
                 }
-                Console.WriteLine();
-                Thread.Sleep(1000);
+                foreach (NetworkInterface adapter in sysInfo.adapters)
+                {
+                    if (adapter.Speed != 0 && adapter.Speed != 1073741824)
+                    {
+                        Console.Write(adapter.NetworkInterfaceType + " ");
+                        Console.Write(adapter.Name + ":\t");
+                        long linkSpeed = adapter.Speed / 1000 / 1000;
+                        string nScale = "Mbps";
+                        if (linkSpeed >= 1000)
+                        {
+                            linkSpeed /= 1000;
+                            nScale = "Gbps";
+                        }
+                        Console.Write("Link Speed: {0}{1}", linkSpeed, nScale);
+                        IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                        UnicastIPAddressInformationCollection UnicastIPAddressInformationCollection = adapterProperties.UnicastAddresses;
+                        string IPv4Adrress = "Not Present", IPv6Address = "Not Present";
+                        foreach (UnicastIPAddressInformation UnicastIPAddressInformation in UnicastIPAddressInformationCollection)
+                        {
+                            if (UnicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
+                                IPv4Adrress = UnicastIPAddressInformation.Address.ToString();
+                            else if (UnicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetworkV6)
+                                IPv6Address = UnicastIPAddressInformation.Address.ToString();
+                        }
+                        Console.Write("\tIPv4 Address: " + IPv4Adrress);
+                        Console.Write("\tIPv6 Address: " + IPv6Address);
+                        Console.WriteLine();
+                    }
+                }
+                Thread.Sleep(1500);
             }
         }
     }
@@ -61,12 +109,16 @@ namespace GetSystemStatus
     {
         private int m_ProcessorCount = 0;   //CPU个数
         private PerformanceCounter pcCpuLoad;   //CPU计数器
-        private PerformanceCounter[] pcCpuCoreLoads;   //每个CPU核心的利用率
-        public PerformanceCounter[] pcDisksRead;
-        private PerformanceCounter pcDiskRead;  //磁盘读速率
-        private PerformanceCounter pcDiskWrite; //磁盘写速率
+        private PerformanceCounter[] pcCpuCoreLoads;   //每CPU核心的利用率
+        private PerformanceCounter[] pcDisksRead;   //每磁盘读速率
+        private PerformanceCounter[] pcDisksWrite;  //每磁盘写速率
+        private PerformanceCounter[] pcDisksLoad;   //磁盘占用率
+        private PerformanceCounter pcDiskRead;  //总磁盘读速率
+        private PerformanceCounter pcDiskWrite; //总磁盘写速率
         private long m_PhysicalMemory = 0;   //物理内存
         public int m_DiskNum = 0;    //磁盘个数
+        public List<string> DiskInstanceNames = new List<string>();
+        public NetworkInterface[] adapters;
 
         private const int GW_HWNDFIRST = 0;
         private const int GW_HWNDNEXT = 2;
@@ -103,12 +155,18 @@ namespace GetSystemStatus
             string[] diskInstanceNames = diskPfc.GetInstanceNames();
             m_DiskNum = diskInstanceNames.Length - 1;
             pcDisksRead = new PerformanceCounter[m_DiskNum];
+            pcDisksWrite = new PerformanceCounter[m_DiskNum];
+            pcDisksLoad = new PerformanceCounter[m_DiskNum];
             int c = 0;
             for(int i = 0; i < diskInstanceNames.Length; i++)
             {
                 if (diskInstanceNames[i] != "_Total")
                 {
-                    pcDisksRead[c++] = new PerformanceCounter("PhysicalDisk", "Disk Read Bytes/sec", diskInstanceNames[i]);
+                    pcDisksRead[c] = new PerformanceCounter("PhysicalDisk", "Disk Read Bytes/sec", diskInstanceNames[i]);
+                    pcDisksWrite[c] = new PerformanceCounter("PhysicalDisk", "Disk Write Bytes/sec", diskInstanceNames[i]);
+                    pcDisksLoad[c] = new PerformanceCounter("PhysicalDisk", "% Disk Time", diskInstanceNames[i]);
+                    DiskInstanceNames.Add(diskInstanceNames[i]);
+                    c++;
                 }
             }
             pcCpuCoreLoads = new PerformanceCounter[m_ProcessorCount];
@@ -122,7 +180,7 @@ namespace GetSystemStatus
             pcDiskRead.NextValue();
             pcDiskWrite.NextValue();
             for (int i = 0; i < m_ProcessorCount; i++) pcCpuCoreLoads[i].NextValue();
-            for (int i = 0; i < m_DiskNum; i++) pcDisksRead[i].NextValue();
+            for (int i = 0; i < m_DiskNum; i++) { pcDisksRead[i].NextValue(); pcDisksWrite[i].NextValue(); pcDisksLoad[i].NextValue(); }
 
             //获得物理内存
             ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
@@ -134,7 +192,11 @@ namespace GetSystemStatus
                     m_PhysicalMemory = long.Parse(mo["TotalPhysicalMemory"].ToString());
                 }
             }
+
+            adapters = NetworkInterface.GetAllNetworkInterfaces();
         }
+
+        
 
         #region CPU个数
         public int ProcessorCount
@@ -170,6 +232,14 @@ namespace GetSystemStatus
         public float DiskRead(int diskId)
         {
             return pcDisksRead[diskId].NextValue();
+        }
+        public float DiskWrite(int diskId)
+        {
+            return pcDisksWrite[diskId].NextValue();
+        }
+        public float DiskLoad(int diskId)
+        {
+            return pcDisksLoad[diskId].NextValue();
         }
         #endregion
 
@@ -212,8 +282,42 @@ namespace GetSystemStatus
             }
         }
         #endregion
-        
+
         /*
+        private void ShowAdapterInfo()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            lst_NetworkAdapter.Items.Add("适配器个数：" + adapters.Length);
+            int index = 0;
+
+            foreach (NetworkInterface adapter in adapters)
+            {
+                index++;
+                //显示网络适配器描述信息、名称、类型、速度、MAC 地址  
+                lst_NetworkAdapter.Items.Add("---------------------第" + index + "个适配器信息---------------------");
+                lst_NetworkAdapter.Items.Add("描述信息：" + adapter.Name);
+                lst_NetworkAdapter.Items.Add("类型：" + adapter.NetworkInterfaceType);
+                lst_NetworkAdapter.Items.Add("速度：" + adapter.Speed / 1000 / 1000 + "MB");
+                lst_NetworkAdapter.Items.Add("MAC 地址：" + adapter.GetPhysicalAddress());
+
+                //获取IPInterfaceProperties实例  
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+
+                //获取并显示DNS服务器IP地址信息  
+                IPAddressCollection dnsServers = adapterProperties.DnsAddresses;
+                if (dnsServers.Count > 0)
+                {
+                    foreach (IPAddress dns in dnsServers)
+                    {
+                        lst_NetworkAdapter.Items.Add("DNS 服务器IP地址：" + dns + "\n");
+                    }
+                }
+                else
+                {
+                    lst_NetworkAdapter.Items.Add("DNS 服务器IP地址：" + "\n");
+                }
+            }
+        }
         #region 获得分区信息 
         ///  
         /// 获取分区信息 
